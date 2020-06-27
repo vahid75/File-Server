4d94adb06521320ae1ad8e670b0d0abd30cfb681
Iman Seyed <seyedrpm1219@gmail.com>
2020-06-27 04:32:44 +0430

    Some changes for making setup script usable.

--- config.sh
+++ config.sh
@@ -1,11 +1,31 @@
-sudo apt install python3 nginx
-python_path=$(which python3)
-virtualenv .venv -p $python_path
+#!/usr/bin/env bash
+
+PYTHON_PATH=$(which python3)
+FILE_SERVER_OWNER=$USER
+PKGS=("nginx" "python3" "virtualenv" "pip3")
+
+function detect_pkg() {
+        CK_PKGS=("$@")
+        for i in "${CK_PKGS[@]}";do
+                echo -n "Detecting $i package: "
+                if [ ! which $i > /dev/null 2>&1 ];then
+                        echo "FAILD."
+                        echo "Hint: Install '$i' with your own package manager and try again."
+                        exit -1
+                else
+                        echo "DONE."
+                fi
+        done
+}
+
+detect_pkg "${PKGS[@]}"
+
+virtualenv .venv -p $PYTHON_PATH
 source .venv/bin/activate
 pip install -r requierments.txt
 touch .env
-echo "export FILE_SERVER_PATH='/home/vahid/file-server/'" > .env
-mkdir /home/vahid/file-server/
+echo "export FILE_SERVER_PATH='/home/$USER/file-server/'" > .env
+mkdir /home/$USER/file-server/
 python3 .ask_username.py
 source .env
 sudo touch /etc/nginx/sites-available/file-server
@@ -15,5 +35,4 @@ sudo chown $FILE_SERVER_OWNER /etc/nginx/sites-enabled/file-server
 chmod 644 /etc/nginx/sites-available/file-server 
 chmod 644 /etc/nginx/sites-enabled/file-server 
 
-echo "All done:))"
-
+echo "Done."
